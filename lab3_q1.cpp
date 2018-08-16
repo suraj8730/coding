9f5cf6c3e75d2e866606e27c8a388d41aead14ea
suraj8730 <42140899+suraj8730@users.noreply.github.com>
2018-08-16 20:25:47 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,18 @@
+// first include library
+# include <iostream>
+using namespace std;
+// main function
+int main(){
+// declaring basic data type
+int a = 2;
+float b= 2.3;
+char c= 'd';
+double d = 2.4654;
+bool e= false;
+// Displaying the sizes of data types. 
+cout << "Size of int=" << sizeof(a) << endl;
+cout << "size of float = " << sizeof(b)<< endl;
+cout << "Size of char = " << sizeof(c) << endl;
+cout << "Size of doubleVar = " << sizeof(d) << endl;
+cout << "Size of boolVar = " << sizeof(e) << endl;
+}
---
+++ lab3_q2.cpp
@@ -0,0 +1,17 @@
+// First Include the library
+# include <iostream>
+using namespace std;
+// Write the main function 
+int main(){
+//declaring the variables
+int a, b;
+// assigning values to variables
+a=7;
+b=2;
+//doing operation and displaying the result on the terminal
+cout << "sum is =" << a+b << endl;
+cout << "subtraction is =" << a-b << endl;
+cout << "value of multiplication =" << a*b << endl;
+cout << "value of divission =" << a/b << endl;
+cout << "modulus is " << a%b << endl;
+}
